4f5e35699c5ec64418e33bf69b91b5fd90026d8b
menghan18 <menghan18@users.noreply.github.com>
2019-04-03 10:34:00 +0800

    bubbleSortChange

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -3,8 +3,8 @@
 function bubbleSort(array) {
   if (!Array.isArray(array))
     return false
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - 1 - i; j++) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
       if (array[j] > array[j + 1]) {}
       [array[j], array[j + 1]] = [array[j + 1], array[j]]
     }
