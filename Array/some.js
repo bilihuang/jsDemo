0cc9e8824db99ea6f39eb6fedbe7eae36a016af3
menghan18 <menghan18@users.noreply.github.com>
2019-06-04 22:59:02 +0800

    some

---
+++ Array/some.js
@@ -0,0 +1,11 @@
+
+const mySome=function(fn,context){
+  let arr=Array.prototype.slice.call(this)
+  if(!arr.length) return false
+  for(let i=0;i<arr.length;i++){
+    if(!arr.hasOwnProperty(i)) continue
+    let res=fn.call(context,arr[i],i,this)
+    if(res) return true
+  }
+  return false
+}
