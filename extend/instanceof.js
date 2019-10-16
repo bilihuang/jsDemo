e118958d8cfd671ec2680d1be4265c04b5a66fe9
menghan18 <menghan18@users.noreply.github.com>
2019-10-16 11:27:28 +0800

    整理结构及格式化

--- extend/instanceof.js
+++ extend/instanceof.js
@@ -1,10 +1,10 @@
 // 实现instanceof
 // 归遍历 right 参数的原型链，每次和 left 参数作比较，遍历到原型链终点时则返回 false，找到则返回 true
-const myInstanceof=function(left,right){
-  let proto=Object.getPrototypeOf(left)
-  while(true){
-    if(proto==null) return false
-    if(proto===right.prototype) return true
-    proto=Object.getPrototypeOf(proto)
+const myInstanceof = function (left, right) {
+  let proto = Object.getPrototypeOf(left)
+  while (true) {
+    if (proto == null) return false
+    if (proto === right.prototype) return true
+    proto = Object.getPrototypeOf(proto)
   }
 }
